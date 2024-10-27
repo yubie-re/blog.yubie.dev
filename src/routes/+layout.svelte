86415cdd6480c8311b3e77864029daf91bfc2696
yubie-re <24496817+yubie-re@users.noreply.github.com>
2024-10-26 23:35:54 -0400

    change header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
     import "../app.css";
-    import GithubIcon from '~icons/mdi/github'
+    import GithubIcon from '~icons/mdi/github';
 </script>
 
 <div class="p-10">
     <a href="/">
-        <h2 class="text-3xl font-bold tracking-tight">yubie.dev</h2>
+        <h2 class="text-3xl font-bold tracking-tight">blog.yubie.dev</h2>
     </a>
     <h2 class="italic">A blog of interesting things I've encountered</h2>
     <a href="https://github.com/yubie-re">
